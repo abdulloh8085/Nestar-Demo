5141728e35ae160160215e90d37c565fdfbc0062
Abdulloh <abdulloh.im4@gmail.com>
2026-09-12 17:45:06 +0900

    feat: organize and test pages router system

---
+++ pages/property/[productId]/index.tsx
@@ -0,0 +1,8 @@
+import { useRouter } from "next/router";
+const PropertyDetail = () => {
+  const router = useRouter();
+  const { productId } = router.query;
+  return <div>PROPERTY DETAIL</div>;
+};
+
+export default PropertyDetail;
